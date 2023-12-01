41027e5073ff058c366ebf7f08a728cd7707ad32
Yana <cavaljn@icloud.com>
2023-12-01 13:57:16 +0100

    table

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import { Table } from "./Table";
 
 function App() {
-  return 
-  <>
-  <div>Hello</div>
-</>
+  const [cellTable, setCellTable] = useState(null);
+
+  const handleClick = (e) => {
+    setCellTable(e.target.textContent);
+  };
+
+  return (
+    <div>
+      <Table onClick={handleClick} />
+      <div> Cell number: {cellTable}</div>
+    </div>
+  );
 }
 
 export default App;
