9e906d701b1497d698a25db8dc6468f23a6db0a3
Yana <cavaljn@icloud.com>
2023-12-15 19:11:47 +0100

    ProductList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ProductList from "./pages/ProductList/ProductList/ProductList";
+import ProductDetail from "./pages/ProductList/ProductDetail/ProductDetail";
 
 function App() {
-  return 
-  <>
-  <div></div>
-</>
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={ProductList} />
+        <Route path="/product/:productId" component={ProductDetail} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
